81057cfa6bb4176fcc3e25fcd424c3291fc132ef
Rudy <pudding.pearl.tea@gmail.com>
2016-05-04 22:57:14 +0800

    lesson 28 - video selection

--- src/index.js
+++ src/index.js
@@ -13,12 +13,15 @@ class App extends Component {
   constructor(props){
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [] ,
+      selectedVideo: null
+    };
 
 
     YTSearch({key : API_KEY, term: 'surfboards'}, (videos) => {
       //console.log(videos);
-      this.setState({ videos });
+      this.setState({ videos , selectedVideo: videos[0]});
     });
   }
 
@@ -26,8 +29,10 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoDetail video={this.state.videos[0]}/>
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={ selectedVideo => this.setState({selectedVideo}) }
+          videos={this.state.videos} />
       </div>
     )
   }
